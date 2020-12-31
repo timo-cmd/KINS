892c6d19b226482a1f05fa741dd5aeb8fb6c3570
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-12-31 19:40:31 +0100

    Delete edge_type.py

--- src/edge_type.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum, unique, auto
-
-@unique
-class EdgeType(Enum):
-	LOCAL_WIRED = auto()
-	LOCAL_WIRELESS = auto()
-	USB_SHARED = auto()
-	USB_TO_PLC = auto()
-	MAIN_TO_ROUTER = auto()
